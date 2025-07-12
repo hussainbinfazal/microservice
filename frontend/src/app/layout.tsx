036854b6772c2c8e908bd3c7f8b97bde4329a632
hussainbinfazal <mdhussain0610@gmail.com>
2025-07-12 23:53:33 +0530

    Add frontend as normal dir

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const inter = Geist_Mono({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+  title: "Micoroservice",
+  description: "A quickcommerce app based on microservices architecture",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
